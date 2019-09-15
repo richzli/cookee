ff9c8673a8a3c87617bbb9e718d16fc9ea87a4cd
Winston Wang <wang4362@purdue.edu>
2019-09-14 23:29:01 -0400

    cards
    cards implement

---
+++ components/PrintPost.js
@@ -0,0 +1,46 @@
+import React, { component } from 'react';
+import {
+    Image,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TouchableOpacity,
+    View,
+  } from 'react-native';
+  import { Container, Header, Content, Card, CardItem, Thumbnail, Icon, Left, Body, Button } from 'native-base';
+  
+  export function PrintPost(props){
+    return(
+        <Container>
+        <Header />
+        <Content>
+          <Card style={{flex: 0}}>
+            <CardItem>
+              <Left>
+                <Body>
+                  <Text>{props.title}</Text>
+                  <Text note>{props.date}</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem>
+              <Body>
+                <Text>
+                  {props.message}
+                </Text>
+              </Body>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button transparent textStyle={{color: '#87838B'}}>
+                  {/* <Icon name="logo-github" /> */}
+                  <Text>{props.stars} likes</Text>
+                </Button>
+              </Left>
+            </CardItem>
+          </Card>
+        </Content>
+      </Container>
+  );
+}
